6acf0aa99787df68ac597e7ad7eaecc8eaf0407f
Mingyang Gao <gaomingyangcn@gmail.com>
2023-10-28 21:12:22 -0600

    完成

---
+++ README.md
@@ -0,0 +1,11 @@
+# Markdown Previewer
+
+## Requirement
+[Build a Markdown Previewer](https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-markdown-previewer)
+
+## Technologies
+* use React build the app
+
+
+## Refer
+https://github.com/markedjs/marked
--- src/App.js
+++ src/App.js
@@ -1,15 +1,76 @@
+import { useState,useEffect } from 'react';
+import {marked} from 'marked';
+
 import './App.css';
 
 function App() {
+  const [leftWidth, setLeftWidth] = useState('50%');
+
+  const handleDrag = (e) => {
+    const container = document.getElementById('container');
+    const containerRect = container.getBoundingClientRect();
+    const newLeftWidth = (e.clientX - containerRect.left) / containerRect.width * 100 + '%';
+    setLeftWidth(newLeftWidth);
+  };
+
+  // const initText="## Welcome to my React Markdown Previewer! \n## This is a sub-heading.\n### And here's some other cool stuff:\nHeres some code, `<div></div>`, between 2 backticks.```// this is multi-line code:\n function anotherExample(firstLine, lastLine) {if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n }\n}```";
+  const initPreview = marked.parse("")
+
+  const [text,setText] = useState("");
+  const [preview,setPreview] = useState(initPreview);
+  
+  
+  useEffect(()=>{
+    console.log("use effect init")
+    const filePath = "/initdata.txt"
+    fetch(filePath)
+      .then((response)=>{
+        if (!response.ok){
+          throw new Error("Network response was not ok")
+        }
+        return response.text()
+      })
+      .then((data)=>{
+        console.log(data)
+        setText(data)
+        let preview = marked.parse(data)
+        setPreview(preview)
+      })
+  },[])
+
+  function changeText(event) {
+    let text = event.target.value
+    setText(text)
+    let html = marked.parse(text);
+    setPreview(html)
+  }
+
   return (
     <div className="App">
-      <div id="editor-container">
-        <textarea id="editor" name="editor" rows="4" cols="50">内容</textarea>
+      <div>
+        <h1>Markdown Previewer</h1>
       </div>
-      <div id="preview-container">
-        <div id="preview">
-          预览
+      <div id='container'>
+        <div id="editor-container" style={{ width: leftWidth }}>
+          <textarea id="editor" onChange={changeText} defaultValue={text}></textarea>
         </div>
+
+        <div className="divider" onMouseDown={(e) => {
+              e.preventDefault();
+              document.addEventListener('mousemove', handleDrag);
+              document.addEventListener('mouseup', () => {
+                document.removeEventListener('mousemove', handleDrag);
+              });
+        }}/>
+
+        <div id="preview-container" style={{ width: `calc(100% - ${leftWidth})` }}>
+          <div id="preview" dangerouslySetInnerHTML={{ __html: preview }}>
+          </div>
+        </div> 
+
+      </div>
+      <div id="footer">
+        by <a href='https://github.com/gaomingyang/markdown-previewer'>Mingyang</a>
       </div>
     </div>
   );
